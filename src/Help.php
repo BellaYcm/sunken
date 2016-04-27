2d607dcded7dc36c152674ddf26f0af24be142ec
sunke <sunkejl@sina.com>
2016-04-27 22:07:21 +0800

    Initial commit

---
+++ src/Help.php
@@ -0,0 +1,22 @@
+<?php
+namespace Sunken\Help;
+/**
+ * Created by PhpStorm.
+ * User: hc
+ * Date: 16/4/27
+ * Time: 下午10:01
+ */
+
+class Help
+{
+    public function __construct()
+    {
+
+    }
+
+    public function test()
+    {
+        return 123;
+    }
+
+}
